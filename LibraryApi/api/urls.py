8809cbc910a808b4a21f23fff457641c7e1e9c04
MaybeNotArnav <arnavmejari@gmail.com>
2022-12-04 17:21:01 +0530

    JWT token setup

--- LibraryApi/api/urls.py
+++ LibraryApi/api/urls.py
@@ -2,10 +2,19 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
- 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns=[
-    path('bookshelf/',views.bookshelf),
-    path('bookshelf/<title>',views.book_read)
+    path('',views.bookshelf),
+    path('add/',views.book_create),
+    path('<title>',views.book_read),
+    path('update/<title>',views.book_update),
+    path('delete/<title>',views.book_delete),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- LibraryApi/api/views.py
+++ LibraryApi/api/views.py
@@ -4,35 +4,62 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.parsers import JSONParser
 from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
-@csrf_exempt
+@api_view(['GET'])
 def bookshelf(request):
-    if request.method == 'GET':
-        book =Book.objects.all()
-        serializer = BookSerializer(book,many=True)
-        return JsonResponse(serializer.data,safe =False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BookSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data,status=201)
-        return JsonResponse(serializer.errors,status=400)
+    book =Book.objects.all()
+    serializer = BookSerializer(book,many=True)
+    return Response(serializer.data)
 
 @csrf_exempt
+@api_view(['POST'])
+def book_create(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
 def book_read(request,title):
     try:
         book = Book.objects.get(title=title)
     except Book.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = BookSerializer(book)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
         
-    elif request.method == 'DELETE':
+@api_view(['PUT'])
+def book_update(request,title):
+    try:
+        book = Book.objects.get(title=title)
+    except Book.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def book_delete(request,title):
+    try:
+        book = Book.objects.get(title=title)
+    except Book.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
         book.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
