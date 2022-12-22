0755db400ff418ffc66291feec8536f273e96cc4
MaybeNotArnav <arnavmejari@gmail.com>
2022-12-22 17:56:36 +0530

    Initial commit

--- LibraryApi/api/urls.py
+++ LibraryApi/api/urls.py
@@ -2,20 +2,16 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import MyTokenObtainPairView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
-urlpatterns=[
-    path('',views.bookshelf),
-    path('add/',views.book_create),
-    path('<title>',views.book_read),
-    path('update/<title>',views.book_update),
-    path('delete/<title>',views.book_delete),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+urlpatterns = [
+    path('', views.bookshelf),
+    path('add/', views.book_create),
+    path('<title>', views.book_read),
+    path('update/<title>', views.book_update),
+    path('delete/<title>', views.book_delete),
+    path('signup/',views.register),
+    path('waiting/',views.wait,name='wait'),
+    path('signup/<token>',views.confirmation)
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
